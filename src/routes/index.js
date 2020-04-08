ab7356764c5a992832550e0ffa8ab1f80234bcf7
angga5895 <1927angga@gmail.com>
2020-04-08 18:41:39 +0700

    08/04/2020 :: [Revisi Fixing] Update Url Router

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,15 @@ import asyncComponent from "util/asyncComponent";
 const App = ({match}) => (
   <div className="gx-main-content-wrapper">
     <Switch>
-        <Route path={`${match.url}#/home`} component={asyncComponent(() => import('./HomePage'))}/>
-        <Route path={`${match.url}#/dashboard/riskprofile`} component={asyncComponent(() => import('./DashboardPage/RiskProfile'))}/>
-        <Route path={`${match.url}#/dashboard/grafikuser`} component={asyncComponent(() => import('./DashboardPage/GrafikUser'))}/>
-        <Route path={`${match.url}#/dashboard/grafikperingkatkonsolidasi`} component={asyncComponent(() => import('./DashboardPage/GrafikKonsolidasi'))}/>
-        <Route path={`${match.url}#/masterdata/jenisrisiko`} component={asyncComponent(() => import('./MasterDataPage/JenisRisiko'))}/>
-        <Route path={`${match.url}#/masterdata/jenispenilaian`} component={asyncComponent(() => import('./MasterDataPage/JenisPenilaian'))}/>
-        <Route path={`${match.url}#/masterdata/jenisperingkatrisiko`} component={asyncComponent(() => import('./MasterDataPage/JenisPeringkatRisiko'))}/>
-        <Route path={`${match.url}#/masterdata/faktor`} component={asyncComponent(() => import('./MasterDataPage/Faktor'))}/>
-        <Route path={`${match.url}#/masterdata/parameter`} component={asyncComponent(() => import('./MasterDataPage/Parameter'))}/>
+        <Route path={`${match.url}home`} component={asyncComponent(() => import('./HomePage'))}/>
+        <Route path={`${match.url}dashboard/riskprofile`} component={asyncComponent(() => import('./DashboardPage/RiskProfile'))}/>
+        <Route path={`${match.url}dashboard/grafikuser`} component={asyncComponent(() => import('./DashboardPage/GrafikUser'))}/>
+        <Route path={`${match.url}dashboard/grafikperingkatkonsolidasi`} component={asyncComponent(() => import('./DashboardPage/GrafikKonsolidasi'))}/>
+        <Route path={`${match.url}masterdata/jenisrisiko`} component={asyncComponent(() => import('./MasterDataPage/JenisRisiko'))}/>
+        <Route path={`${match.url}masterdata/jenispenilaian`} component={asyncComponent(() => import('./MasterDataPage/JenisPenilaian'))}/>
+        <Route path={`${match.url}masterdata/jenisperingkatrisiko`} component={asyncComponent(() => import('./MasterDataPage/JenisPeringkatRisiko'))}/>
+        <Route path={`${match.url}masterdata/faktor`} component={asyncComponent(() => import('./MasterDataPage/Faktor'))}/>
+        <Route path={`${match.url}masterdata/parameter`} component={asyncComponent(() => import('./MasterDataPage/Parameter'))}/>
     </Switch>
   </div>
 );
