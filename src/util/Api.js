ce225d786dbc68d24b79f977aff2c364002b3673
rizkisadewa <rizkykysadewa@gmail.com>
2020-04-20 10:47:43 +0700

    merged and adjusted from branch server

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://${window.location.hostname}:3001/`,
+  baseURL: `http://172.31.101.127:3001/`,
   headers: {
     'Content-Type': 'application/json',
   }
