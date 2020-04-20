1e41d7953753a8ec312bf89202af4178db72689c
rizkisadewa <rizkykysadewa@gmail.com>
2020-04-20 09:52:09 +0700

    initial deploy to server

--- src/util/Api.js
+++ src/util/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://45.251.74.165:3001/`,
+  baseURL: `http://${window.location.hostname}3001/`,
   headers: {
     'Content-Type': 'application/json',
   }
